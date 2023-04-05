6c71fea5976d9d91c0930fc1f0f56d19394beb84
Amptyz <a542674348@gmail.com>
2023-04-05 15:51:01 +0800

    jieba分词，文本处理

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Wenbenchuli test =new Wenbenchuli();
-        test.GetString_analyse("result，我想请问一下王同学。你答得很好，再接再厉啊！你来说说看，他是不是傻逼啊？真是个蠢蛋");
+        test.GetString_analyse("我想请问一下王同学。你答得很好，再接再厉啊！你来说说看，他是不是傻逼啊？真是个蠢蛋");
         test.show_result();
 
     }
